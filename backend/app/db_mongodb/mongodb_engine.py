89ce32e5288b4d6ba042a89a9ad75a358f6929ac
jasonChenTwYo <a60509561@gmail.com>
2024-02-19 11:25:36 +0800

    update

--- backend/app/db_mongodb/mongodb_engine.py
+++ backend/app/db_mongodb/mongodb_engine.py
@@ -1,13 +1,15 @@
 from odmantic import AIOEngine, SyncEngine
 from motor.motor_asyncio import AsyncIOMotorClient
 from pymongo import MongoClient
+import os
 
-user = "appuser"
-passward = "1234567"
-db = "projectOne"
+db = os.getenv("MONGO_INITDB_DATABASE", "projectOne")
+host = os.getenv("MONGODB_HOST", "localhost")
+user = os.getenv("MONGO_USER", "appuser")
+passward = os.getenv("MONGO_PASSWORD", "1234567")
 
-async_client = AsyncIOMotorClient(f"mongodb://{user}:{passward}@localhost:27017/{db}")
+async_client = AsyncIOMotorClient(f"mongodb://{user}:{passward}@{host}:27017/{db}")
 async_engine = AIOEngine(client=async_client, database=db)
 
-sync_client = MongoClient(f"mongodb://{user}:{passward}@localhost:27017/{db}")
+sync_client = MongoClient(f"mongodb://{user}:{passward}@{host}:27017/{db}")
 sync_engine = SyncEngine(client=sync_client, database=db)
--- backend/app/db_mysql/mysql_engine.py
+++ backend/app/db_mysql/mysql_engine.py
@@ -1,18 +1,16 @@
 from sqlmodel import create_engine
 import os
 
-environment = os.getenv("ENVIRONMENT", "dev")
-
-database = "projectOne"
-host = "localhost"
-user = "appuser"
-# pf = open('/run/secrets/mysql_password', 'r')
-pf = "1234567"
+database = os.getenv("MYSQL_DATABASE", "projectOne")
+host = os.getenv("MYSQL_HOST", "localhost")
+user = os.getenv("MYSQL_USER", "appuser")
+with open(os.getenv("MYSQL_PASSWORD_FILE", "../mysql/mysql_password.txt"), "r") as file:
+    mysql_password = file.read().strip()
 # https://docs.sqlalchemy.org/en/14/core/engines.html#database-urls
 # dialect+driver://username:password@host:port/database
 # https://docs.sqlalchemy.org/en/14/core/engines.html#mysql
 # https://docs.sqlalchemy.org/en/14/dialects/mysql.html#module-sqlalchemy.dialects.mysql.mysqlconnector
 # PyMySQL
-DATABASE_URL = f"mysql+pymysql://{user}:{pf}@{host}/{database}"
+DATABASE_URL = f"mysql+pymysql://{user}:{mysql_password}@{host}/{database}"
 
 engine = create_engine(DATABASE_URL, echo=True)
